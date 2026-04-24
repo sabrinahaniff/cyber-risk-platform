71565b44de944fe7aa87bbd5f78c469fbc646bdf
sabrinahaniff <xxcrystalwindxx@gmail.com>
2026-04-23 20:31:37 -0400

    initial project setup

---
+++ src/main/java/com/cyberrisk/cyber_risk_platform/CyberRiskPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cyberrisk.cyber_risk_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberRiskPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberRiskPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyberrisk/cyber_risk_platform/CyberRiskPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberrisk.cyber_risk_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberRiskPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
