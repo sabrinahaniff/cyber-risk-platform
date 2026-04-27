8f8f4c7ecb030b71705e1893725292ba29048679
sabrinahaniff <xxcrystalwindxx@gmail.com>
2026-04-27 14:51:00 -0400

    fix test failing

--- src/test/java/com/cyberrisk/cyber_risk_platform/CyberRiskPlatformApplicationTests.java
+++ src/test/java/com/cyberrisk/cyber_risk_platform/CyberRiskPlatformApplicationTests.java
@@ -2,12 +2,13 @@ package com.cyberrisk.cyber_risk_platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CyberRiskPlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
